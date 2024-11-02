eed38ddd1f59fd4e4b0b660ea878552c77d26b09
fernandevx <ortizfernando05@gmail.com>
2024-11-02 12:49:11 -0600

    feat: add cron service

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,10 @@
+import { CronService } from '@/presentation/cron/cron-service';
+
 export class Server {
   public static start() {
-    console.log('Server estarted...');
+    CronService.createJob('*/5 * * * * *', () => {
+      const date = new Date();
+      console.log('5 seconds', date);
+    });
   }
 }
