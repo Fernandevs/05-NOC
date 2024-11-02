21b6ac8869f34eeded925f44d6493112a1f2f538
fernandevx <ortizfernando05@gmail.com>
2024-11-02 16:37:51 -0600

    feat: add check service use case

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,10 +1,15 @@
+import { CheckService } from '@/domain/use-cases/checks/ckeck-service';
 import { CronService } from '@/presentation/cron/cron-service';
 
 export class Server {
   public static start() {
     CronService.createJob('*/5 * * * * *', () => {
-      const date = new Date();
-      console.log('5 seconds', date);
+      const url = 'http://google.com';
+      new CheckService(
+        () => console.log(`${url} is ok`),
+        (error) => console.error(error)
+      ).execute(url);
+      // new CheckService().execute('http://localhost:3000');
     });
   }
 }
