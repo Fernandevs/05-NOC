dace39db88f2723d4fe42c01d1c13c18fe5ebd19
fernandevx <ortizfernando05@gmail.com>
2024-11-02 18:15:15 -0600

    feat: repository and datasource implementation

--- src/infrastructure/datasources/file-system.datasource.ts
+++ src/infrastructure/datasources/file-system.datasource.ts
@@ -4,7 +4,7 @@ import { LogEntity, LogSeverityLevel } from '@/domain/entities/log.entity';
 
 export class FileSystemDatasource extends LogDatasource {
   private readonly logPath = 'logs/';
-  private readonly allLogsPath = 'logs/logs-low.log';
+  private readonly allLogsPath = 'logs/logs-all.log';
   private readonly mediumLogsPath = 'logs/logs-medium.log';
   private readonly highLogsPath = 'logs/logs-high.log';
 
@@ -26,10 +26,35 @@ export class FileSystemDatasource extends LogDatasource {
     );
   };
 
-  saveLog(log: LogEntity): Promise<void> {
-    throw new Error('Method not implemented.');
+  async saveLog(newLog: LogEntity): Promise<void> {
+    const logAsJson = `${JSON.stringify(newLog)}\n`;
+    fs.appendFileSync(this.allLogsPath, logAsJson);
+
+    switch (newLog.level) {
+      case LogSeverityLevel.medium:
+        fs.appendFileSync(this.mediumLogsPath, logAsJson);
+        break;
+      case LogSeverityLevel.high:
+        fs.appendFileSync(this.highLogsPath, logAsJson);
+        break;
+    }
   }
-  getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
-    throw new Error('Method not implemented.');
+
+  private getLogsFromFile = (path: string): LogEntity[] => {
+    const content = fs.readFileSync(path, 'utf-8');
+    return content.split('\n').map((log) => LogEntity.fromJson(log));
+  };
+
+  async getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
+    switch (severityLevel) {
+      case LogSeverityLevel.low:
+        return this.getLogsFromFile(this.allLogsPath);
+      case LogSeverityLevel.medium:
+        return this.getLogsFromFile(this.mediumLogsPath);
+      case LogSeverityLevel.high:
+        return this.getLogsFromFile(this.highLogsPath);
+      default:
+        throw new Error(`${severityLevel} not implemented`);
+    }
   }
 }
